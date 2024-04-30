f39f5348d26e5911bc0c7e79d4257fc7650a1f68
Genevieve Will <genefwill@gmail.com>
2024-04-30 15:08:26 -0700

    Cleaning data

--- movie.py
+++ movie.py
@@ -1,22 +1,36 @@
-
 import numpy as np
 import pandas as pd
 import os
 import random
+import spacy
+import re
 
 
+# Load English language model from spaCy
+nlp = spacy.load('en_core_web_lg')
 
-def load_imdb_sentiment_analysis_dataset():
+# Load IMDb dataset
+data = pd.read_csv("data/imdb_dataset.csv")
+cleaned_data = data[["review", "sentiment"]]
 
-    training_data = pd.read_csv("data\\imdb_dataset.csv")
+# Tokenize stopwords using spaCy
+stopwords = nlp.Defaults.stop_words
 
-    texts = []
-    cleaned_data = training_data[["review", "sentiment"]]
-    print(len(cleaned_data))
-    
-    
+# Initialize lists to store processed data
+docs = []
+y = []
+
+# Process each row in the dataset. cleaning data
+# remove stop words and html tags
+for index, row in cleaned_data.iterrows():
+    review = row["review"]
+    review = re.sub(r'[^a-zA-Z ]', '', review)
+    tokens = nlp(review)
+    sentiment = row["sentiment"]
+    gen = row["sentiment"]
+    if (110 < len(tokens) <= 500):
+        y.append(gen)
+        nsw_tokens = [token.lemma_ for token in tokens if not token.text in stopwords]
+        docs.append(" ".join(nsw_tokens))
 
 
-
-load_imdb_sentiment_analysis_dataset()
-    
